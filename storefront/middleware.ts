8fdb52e0ad31c354b037b8a11dc5aa9c40249416
foolish66776 <manager.tfb@gmail.com>
2026-05-04 02:20:27 +0200

    fix: simpler middleware - delete x-forwarded-port entirely

--- storefront/middleware.ts
+++ storefront/middleware.ts
@@ -6,20 +6,11 @@ const intlMiddleware = createNextIntlMiddleware(routing)
 
 export default function middleware(request: NextRequest) {
   const headers = new Headers(request.headers)
-
   const forwardedHost = headers.get('x-forwarded-host')
-  if (forwardedHost) {
-    if (forwardedHost.includes(':')) {
-      headers.set('x-forwarded-host', forwardedHost.split(':')[0])
-    }
-  } else {
-    headers.set('x-forwarded-host', request.headers.get('host')?.split(':')[0] ?? '')
+  if (forwardedHost && forwardedHost.includes(':')) {
+    headers.set('x-forwarded-host', forwardedHost.split(':')[0])
   }
-
-  if (!headers.get('x-forwarded-port')) {
-    headers.set('x-forwarded-port', '443')
-  }
-
+  headers.delete('x-forwarded-port')
   return intlMiddleware(request)
 }
 
