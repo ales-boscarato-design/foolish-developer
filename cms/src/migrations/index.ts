c0f26fdf88413fcdd397849c8790a76a9997e857
foolish66776 <manager.tfb@gmail.com>
2026-05-10 22:54:35 +0200

    chore(cms): remove old migration to force fresh schema generation

--- cms/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260426_194514 from './20260426_194514';
-
-export const migrations = [
-  {
-    up: migration_20260426_194514.up,
-    down: migration_20260426_194514.down,
-    name: '20260426_194514'
-  },
-];
