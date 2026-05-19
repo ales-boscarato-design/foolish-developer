88b1426f6162f55701ecf3bd477a6a0cba4d9bdc
foolish66776 <manager.tfb@gmail.com>
2026-05-19 12:46:31 +0200

    Fix: remove bad migration files, keep only 20260519 migration

--- cms/src/migrations/index.ts
+++ cms/src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_002_add_stencil_column from './002_add_stencil_column';
-import * as migration_20260519_102507_add_stencil_column from './20260519_102507_add_stencil_column';
+import * as migration_20260519_103000_add_stencil_column from './20260519_103000_add_stencil_column';
 
 export const migrations = [
   {
-    up: migration_002_add_stencil_column.up,
-    down: migration_002_add_stencil_column.down,
-    name: '002_add_stencil_column',
-  },
-  {
-    up: migration_20260519_102507_add_stencil_column.up,
-    down: migration_20260519_102507_add_stencil_column.down,
-    name: '20260519_102507_add_stencil_column'
+    up: migration_20260519_103000_add_stencil_column.up,
+    down: migration_20260519_103000_add_stencil_column.down,
+    name: '20260519_103000_add_stencil_column'
   },
 ];
